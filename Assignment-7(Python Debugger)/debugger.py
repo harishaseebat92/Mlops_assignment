6a629b598d0ae9a833192883b6d71ee6e0103f4a
harishaseebat92 <harishaseebat92@gmail.com>
2024-11-17 18:02:00 +0530

    Assignment_7 submission

--- Assignment-7(Python Debugger)/debugger.py
+++ Assignment-7(Python Debugger)/debugger.py
@@ -1,13 +1,33 @@
-import pdb
+from sklearn.metrics import mean_squared_error
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-def preprocess_data(data):
-    scaler = StandardScaler()
-    pdb.set_trace() # Start debugger here
-    scaled_data = scaler.fit_transform(data)
-    normalized_data = scaled_data / np.linalg.norm(scaled_data)
-    return normalized_data
+from sklearn.linear_model import LinearRegression
+import pdb
+def train_model(X, y):
+    model = LinearRegression()
+   #  pdb.set_trace() # Debugger for model training
+    model.fit(X, y)
+    return model
 # Example Data
-data = np.array([[1, 2, 3], [4, 5, np.nan], [7, 8, 9]])
-processed_data = preprocess_data(data)
-print("Processed Data:\n", processed_data)
+X = np.array([[1], [2], [3], [4]])
+y = np.array([1, 4, 9, 16, 25]) # Incorrect shape on purpose
+y = y[:X.shape[0]]  # Slice y to keep the first 4 elements
+
+trained_model = train_model(X, y)
+print("Trained Model Coefficients:", trained_model.coef_)
+
+def evaluate_model(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    pdb.set_trace() # Debugger for evaluation
+    mse = mean_squared_error(y_test, predictions)
+    return mse
+X_test = np.array([[5], [6], [7], [8]])
+y_test = np.array([25, 36, 49, 64])
+# Using trained_model from previous task
+mse_score = evaluate_model(trained_model, X_test, y_test)
+print("Mean Squared Error:", mse_score)
+
+# (Pdb) pp predictions
+# array([20., 25., 30., 35.])
+
+# (Pdb) pp y_test
+# array([25, 36, 49, 64])
